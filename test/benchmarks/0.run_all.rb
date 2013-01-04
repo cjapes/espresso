627099266019e21a3e9ebac91f8d581f7f9110d3
Roman Heinrich <roman.heinrich@gmail.com>
2013-01-05 01:20:00 +0200

    benchmark runner

--- test/benchmarks/0.run_all.rb
+++ test/benchmarks/0.run_all.rb
@@ -1,12 +1,47 @@
 #!/usr/bin/env ruby
 
-dir = File.dirname(__FILE__)
-files = Dir["#{dir}/*_benchmark.rb"]
 
-files.each do |f|
-  a = `ruby #{f}`
-  puts f
-  puts "-" * 80
-  puts a
-  puts "-" * 80
+class BenchmarkRunner
+  attr_accessor :rvm_handler
+  def rvm_handler
+    @rvm_handler||= RvmHandler.new
+  end
+
+  def current_dir
+    File.dirname(__FILE__)
+  end
+
+  def benchmark_files
+    @files ||= Dir["#{current_dir}/*_benchmark.rb"]
+  end
+
+  def rubies
+    %w[ree-1.8.7-2012.02  ruby-1.9.3-p327-turbo]
+  end
+
+  def run_file(f)
+    puts f
+    puts "-" * 80
+    puts `ruby #{f}`
+    puts "-" * 80
+    puts
+  end
+
+  def run_suite
+    puts rvm_handler.current_ruby
+    benchmark_files.each do |f|
+      run_file(f)
+    end
+  end
 end
+
+class RvmHandler
+  def current_ruby
+    shell("rvm info|head -n 2")
+  end
+
+  def shell(cmd)
+    IO.popen(cmd) { |f| puts f.gets }
+  end
+end
+BenchmarkRunner.new.run_suite
