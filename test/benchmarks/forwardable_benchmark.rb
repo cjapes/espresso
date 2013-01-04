7e9fa9fc002ed27ea2241f4b0c52ac21681843f1
Roman Heinrich <roman.heinrich@gmail.com>
2013-01-05 00:43:01 +0200

    indentation

--- test/benchmarks/forwardable_benchmark.rb
+++ test/benchmarks/forwardable_benchmark.rb
@@ -24,20 +24,22 @@ end
 
 @subject = Subject.new
 n = 500000
-Benchmark.bm(7) do |x|
+Benchmark.bm(10) do |x|
   x.report("plain:")   { for i in 1..n; @subject.plain_some_method(nil); end }
   x.report("delegator:") { for i in 1..n; @subject.some_method(nil); end }
 end
 
 # ruby-1.9.3-p327-turbo
-#               user     system      total        real
-# plain:    0.760000   0.020000   0.780000 (  0.777407)
-# delegator:  0.760000   0.000000   0.760000 (  0.766489)
+#                  user     system      total        real
+# plain:       0.790000   0.030000   0.820000 (  0.808321)
+# delegator:   0.770000   0.010000   0.780000 (  0.781279)
+
 
 # ree-1.8.7-2012.02
-#              user     system      total        real
-# plain:   1.570000   0.100000   1.670000 (  1.661736)
-# delegator:  1.860000   0.060000   1.920000 (  1.923199)
+#                 user     system      total        real
+# plain:      1.510000   0.060000   1.570000 (  1.578078)
+# delegator:  1.820000   0.070000   1.890000 (  1.880947)
+
 
 ## We see something very interesting here: in ruby 1.9.3 delegation is actually faster, than
 ## plain code! ))
